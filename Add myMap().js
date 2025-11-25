e55f3f25e7c128390b3f8999239018157f823fdd
bhoomishivhare <152948254+bhoomishivhare@users.noreply.github.com>
2025-11-25 15:12:49 +0530

    Create Add myMap().js

---
+++ Add myMap().js
@@ -0,0 +1,13 @@
+// Q8: Custom map()
+"use strict";
+
+Array.prototype.myMap = function(callback) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        result.push(callback(this[i], i, this));
+    }
+    return result;
+};
+
+// Test
+console.log([1,2,3].myMap(n => n * 2)); // [2,4,6]
