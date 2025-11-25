3404c316a843c17ea7622d2f7fb8619af9659d1f
bhoomishivhare <152948254+bhoomishivhare@users.noreply.github.com>
2025-11-25 14:40:12 +0530

    Create Progressive Discount System.js

---
+++ Progressive Discount System.js
@@ -0,0 +1,14 @@
+// Q6. Discount System
+
+let totalPurchase = 6800;
+let discount = 0;
+
+if (totalPurchase >= 10000) discount = 25;
+else if (totalPurchase >= 5000) discount = 15;
+else if (totalPurchase >= 2000) discount = 5;
+
+let finalPrice = totalPurchase - (totalPurchase * discount / 100);
+
+console.log("Original Total:", totalPurchase);
+console.log("Discount %:", discount);
+console.log("Final Price:", Math.round(finalPrice));
