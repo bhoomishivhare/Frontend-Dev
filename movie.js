8f573652f315e96e4790801283d2fe8c711eee19
bhoomishivhare <152948254+bhoomishivhare@users.noreply.github.com>
2025-12-04 11:16:48 +0530

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,26 @@
+class MovieTicket {
+    constructor(movieName, seatNo, price) {
+        this.movieName = movieName;
+        this.seatNo = seatNo;
+        this.price = price;
+    }
+}
+
+MovieTicket.prototype.printTicket = function () {
+    console.log(`Movie: ${this.movieName}, Seat: ${this.seatNo}, Price: ${this.price}`);
+};
+
+class OnlineTicket extends MovieTicket {
+    constructor(movieName, seatNo, price, convenienceFee) {
+        super(movieName, seatNo, price);
+        this.convenienceFee = convenienceFee;
+    }
+
+    getTotalAmount() {
+        return this.price + this.convenienceFee;
+    }
+}
+
+let t1 = new OnlineTicket("Animal", "B12", 250, 50);
+t1.printTicket();
+console.log("Total Amount:", t1.getTotalAmount());
