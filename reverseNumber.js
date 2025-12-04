797c78813eee8390e23a9a9d0156a6287fd5dc2d
bhoomishivhare <152948254+bhoomishivhare@users.noreply.github.com>
2025-12-04 11:10:54 +0530

    Create reverseNumber.js

---
+++ reverseNumber.js
@@ -0,0 +1,10 @@
+function reverseNumber(num) {
+    let rev = 0;
+    while (num > 0) {
+        rev = rev * 10 + (num % 10);
+        num = Math.floor(num / 10);
+    }
+    console.log("Reversed number:", rev);
+}
+
+reverseNumber(9876);
